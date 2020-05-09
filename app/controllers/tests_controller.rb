bc55642ef49f31b4d97157e327d2987abfa29e48
JiaXuan Hon <46828701+idanceinbetween@users.noreply.github.com>
2020-05-09 21:26:12 +0100

    Updates methods in Test Controller (index, show, create)

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,18 +1,29 @@
 class TestsController < ApplicationController
 
     def index
+        tests = Test.all
+        if tests
+            render json: tests
+        else
+            render json: {error: 'There are no tests.'}, status: 404
+        end
     end
 
     def show
-       @test = Test.find(params[:id])
+       test = Test.find_by(id: params[:id])
+       if test
+           render json: test
+       else
+           render json: {error: "Test with ID #{params[:id]} cannot be found."}, status: 404
+       end
     end
 
     def create
-        @test = Test.new(name: params[:name])
-        if @test.save
-            puts "successfully created and saved #{@test.id}"
+        test = Test.new(name: params[:name])
+        if test.save
+          render json: test
         else
-            puts "oh no, couldn't create and save"
+          render json: {error: "Error creating a new test thing."}, status: 404
         end
     end    
 end
